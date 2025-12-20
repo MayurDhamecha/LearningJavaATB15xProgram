c0792d7f4966a5b8b961bb06770580acb7374213
Mayur Dhamecha <dhamechamayur234@gmail.com>
2025-12-20 13:05:45 +0530

    Datatypes and Variables

--- src/ex_01_Java_Basics/Lab002_Datatypes_And_Variables.java
+++ src/ex_01_Java_Basics/Lab002_Datatypes_And_Variables.java
@@ -4,7 +4,21 @@ public class Lab002_Datatypes_And_Variables {
     public static void main(String[] args) {
 
         // Local Variables - Which only define within a method/function
-        byte byte_Variable = 40;
+        int int_Variable = 10;
+        System.out.println("Byte variable value is : " + int_Variable);
+        short short_Variable = 20;
+        System.out.println("Byte variable value is : " + short_Variable);
+        long long_Variable = 30;
+        System.out.println("Byte variable value is : " + long_Variable);
+        float float_Variable = 40;
+        System.out.println("Byte variable value is : " + float_Variable);
+        double double_Variable = 50;
+        System.out.println("Byte variable value is : " + double_Variable);
+        boolean boolean_Variable = true;
+        System.out.println("Byte variable value is : " + boolean_Variable);
+        char char_Variable = 'a';
+        System.out.println("Byte variable value is : " + char_Variable);
+        byte byte_Variable = 60;
         System.out.println("Byte variable value is : " + byte_Variable);
     }
 }
