646d7b90b218d6bd11a9431eddaeeda0f9307bbd
Mayur Dhamecha <dhamechamayur234@gmail.com>
2025-12-20 11:39:23 +0530

    Task Commit for 17th Dec 2025

---
+++ src/ex_01_Java_Basics/Lab002_Datatypes_And_Variables.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Datatypes_And_Variables {
+    public static void main(String[] args) {
+
+        // Local Variables - Which only define within a method/function
+        byte byte_Variable = 40;
+        System.out.println("Byte variable value is : " + byte_Variable);
+    }
+}
---
+++ src/ex_01_Java_Basics/Task_17_Print_Table_9_Using_Printf.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Task_17_Print_Table_9_Using_Printf {
+    public static void main(String[] args) {
+        int tableOf9 = 9;
+        System.out.printf("%d X 1 = %d%n", tableOf9, tableOf9 * 1);
+        System.out.printf("%d X 2 = %d%n", tableOf9, tableOf9 * 2);
+        System.out.printf("%d X 3 = %d%n", tableOf9, tableOf9 * 3);
+        System.out.printf("%d X 4 = %d%n", tableOf9, tableOf9 * 4);
+        System.out.printf("%d X 5 = %d%n", tableOf9, tableOf9 * 5);
+        System.out.printf("%d X 6 = %d%n", tableOf9, tableOf9 * 6);
+        System.out.printf("%d X 7 = %d%n", tableOf9, tableOf9 * 7);
+        System.out.printf("%d X 8 = %d%n", tableOf9, tableOf9 * 8);
+        System.out.printf("%d X 9 = %d%n", tableOf9, tableOf9 * 9);
+        System.out.printf("%d X 10 = %d%n", tableOf9, tableOf9 * 10);
+    }
+}
